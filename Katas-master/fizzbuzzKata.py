4eea562da95a2f84172856dd177c67feec593f67
Henrry96 <alumno.portillo@gmail.com>
2014-10-29 15:36:39 -0700

    Revert "Revert "str2""
    This reverts commit b0168a213322f4cb7f1f1cf22fa1fea79591414d.

--- Katas-master/fizzbuzzKata.py
+++ Katas-master/fizzbuzzKata.py
@@ -1,10 +1,12 @@
 def fizzbuzz(num):
-    if int(num) % 15 == 0:
+    num = int(num)
+    if (num) % 15 == 0:
         return "fizzbuzz"
-    elif int(num) % 5 == 0:
+    elif (num) % 5 == 0:
         return "buzz"
-    elif int(num) % 3 == 0:
+    elif (num) % 3 == 0:
         return "fizz"
-    return str(num)
+    else:
+        return str(num)
 
     raise NotImplementedError
